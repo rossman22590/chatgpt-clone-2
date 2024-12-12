6f194dc9f1865582ecc80b48c424f034cdd5f96c
Ross Cohen <rosscohen1990@gmail.com>
2024-12-12 15:08:01 -0500

    Update AuthLayout.tsx

--- client/src/components/Auth/AuthLayout.tsx
+++ client/src/components/Auth/AuthLayout.tsx
@@ -60,7 +60,7 @@ function AuthLayout({
       <Banner />
       <BlinkAnimation active={isFetching}>
         <div className="mt-6 h-10 w-full bg-cover">
-          <img src="/assets/logo.svg" className="h-full w-full object-contain" alt="Logo" />
+           <img src="https://img.mytsi.org/i/gxDs711.png" className="h-full w-full object-contain" alt="Logo" />
         </div>
       </BlinkAnimation>
       <DisplayError />
